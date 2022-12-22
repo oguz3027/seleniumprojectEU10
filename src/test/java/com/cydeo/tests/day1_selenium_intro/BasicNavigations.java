8f83f8cc5d9e3f18f8db5dc83bed5f32b7d245ad
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2022-12-22 19:30:03 +0300

    SeleniumTest and BasicNavigations classes

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,49 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.tesla.com");
+
+        driver.get("https://www.google.com");
+
+        String currentTitle = driver.getTitle();
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentTitle = " + currentTitle);
+        System.out.println("currentURL = " + currentURL);
+
+
+
+
+        Thread.sleep(2000);
+
+        driver.navigate().back();
+
+        Thread.sleep(2000);
+
+        driver.navigate().forward();
+
+        driver.navigate().refresh();
+
+        driver.navigate().to("https://www.facebook.com");
+
+        currentTitle = driver.getTitle();
+
+        //System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        System.out.println("currentTitle = " + currentTitle);
+
+        currentURL = driver.getCurrentUrl();
+
+        System.out.println("currentURL = " + currentURL);
+
+    }
+}
