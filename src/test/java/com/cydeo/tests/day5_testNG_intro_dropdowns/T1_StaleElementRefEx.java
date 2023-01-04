9c2da0005d346ddb92a6838915aae135daba6ffe
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 00:23:20 +0300

    Day5_T1 created

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_StaleElementRefEx {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        WebDriverFactory.maximize(driver);
+
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverFactory.wait(driver);
+
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
+        WebElement addElementButton = driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        Thread.sleep(2000);
+
+        addElementButton.click();
+
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
+
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
+        deleteButton.click();
+
+        try {
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+        }catch (StaleElementReferenceException e){
+            System.out.println("--> StaleElementReferenceException exception is thrown");
+            System.out.println("-->This means the web element is completely deleted from the page");
+            System.out.println("deleteButton.isDisplayed() = false");
+        }
+
+        driver.close();
+
+    }
+}
