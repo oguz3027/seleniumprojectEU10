2d38c40b7c35d0d6a077d1053cc9d9afdf15385e
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 00:23:45 +0300

    Day5_T2 created

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T2_RadioButton {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        WebDriverFactory.maximize(driver);
+
+        WebDriverFactory.wait(driver);
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        WebElement hockeyButton = driver.findElement(By.xpath("//input[@id='hockey']"));
+
+        System.out.println("hockeyButton.isSelected() = " + hockeyButton.isSelected());
+
+        hockeyButton.click();
+
+        System.out.println("hockeyButton.isSelected() = " + hockeyButton.isSelected());
+
+        if(hockeyButton.isSelected()){
+            System.out.println("Butoon is selected. Verification PASSED");
+        }else{
+            System.out.println("Butoon is not selected. Verification FAILED");
+        }
+
+    }
+}
