48b84a15d2be854286756c126052359e62f52a7c
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 11:47:36 +0300

    TestNG_Selenium updated

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -16,7 +16,7 @@ public class TestNG_Selenium {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
 
-        Assert.assertEquals(actualTitle,expectedTitle);
+        Assert.assertEquals(actualTitle,expectedTitle,"Title is not matching here");
 
     }
 }
