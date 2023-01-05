0b7a6da731761474cd73b55aa2eb9d517a3b60af
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 11:43:58 +0300

    TestNG_Intro updated

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,23 +1,38 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setUpClass(){
+        System.out.println("---->BeforeClass is running");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("---->AfterClass is running");
+    }
 
     @BeforeMethod
     public void setUpMethod(){
         System.out.println("-->BeforeMethod is running!");
     }
     @AfterMethod
-    public void tearDown(){
+    public void tearDownMethod(){
         System.out.println("-->AfterMethod is running!");
     }
 
     @Test (priority = 2)
     public void test1(){
         System.out.println("Test 1 is running...");
+
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertTrue(actual.equals(expected));
+
     }
 
     @Test (priority = 1)
