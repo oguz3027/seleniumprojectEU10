404e140b1e1012e263b9f51f6ed2a034d05685e8
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 12:38:46 +0300

    TestNG_Selenium updated

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class T4_SimpleDropdowns {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+    }
+
+    @Test
+    public void simpleDropdownTest(){
+
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+        WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+
+        String actualSimpleDropdownText = currentlySelectedOption.getText();
+        String expectedSimpleDropdownText = "Please select an option";
+
+        Assert.assertEquals(actualSimpleDropdownText,expectedSimpleDropdownText,"It's not equal");
+
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+        String actualStateDropdownText = stateDropdown.getFirstSelectedOption().getText();
+        String expectedStateDropdownText = "Select a State";
+
+        Assert.assertEquals(actualStateDropdownText,expectedStateDropdownText);
+
+
+    }
+}
--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -3,12 +3,26 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Selenium {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+    }
 
     @Test
-    public void selenium_test(){
+    public void selenium_test() {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         WebDriverFactory.maximize(driver);
         WebDriverFactory.implicitlyWait(driver);
@@ -16,7 +30,7 @@ public class TestNG_Selenium {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
 
-        Assert.assertEquals(actualTitle,expectedTitle,"Title is not matching here");
+        Assert.assertEquals(actualTitle, expectedTitle, "Title is not matching here");
 
     }
 }
