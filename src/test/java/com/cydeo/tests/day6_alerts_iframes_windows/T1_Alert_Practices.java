021366bf07fc4cc599259378807caba4e248b7ff
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-05 20:38:36 +0300

    Day 6 Alert Test updated

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,8 +23,24 @@ public class T1_Alert_Practices {
     @Test
     public void alert_test1(){
 
+        driver.get("https://practice.cydeo.com/javascript_alerts");
 
+        WebElement clickForJSAlert = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
 
+        clickForJSAlert.click();
 
-    }
+        Alert alert = driver.switchTo().alert();
+
+        alert.accept();
+
+        WebElement resultText = driver.findElement(By.xpath("//p[.='You successfully clicked an alert']"));
+
+        Assert.assertTrue(resultText.isDisplayed(),"Result text is NOT displayed.");
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+
+        Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
+
+        }
 }
