b7a3c37656ebc81544e748a100fff193a1cc7334
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-13 15:14:19 +0300

    Day 7 T1 window handling updated

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day7_webtables_utilities_javafaker;
 import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -41,6 +42,11 @@ public class T1_WindowHandling {
             }
 
         }
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
 
 
     }
