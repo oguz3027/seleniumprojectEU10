e5aed948112174baf6922e5df740aa495f27d921
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-16 00:33:31 +0300

    Day 7 T3_CRM_LOGIN

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class T3_CRM_LOGIN {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+    }
+
+    @Test
+    public void crm_login_test(){
+        driver.get("https://login1.nextbasecrm.com");
+
+
+
+
+
+
+    }
+}
