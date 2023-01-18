ca1b4901e107dfc26f161440ff24e6279f60f5f7
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-18 13:13:26 +0300

    Day 9 Driver

--- src/test/java/com/cydeo/tests/utilities/Driver.java
+++ src/test/java/com/cydeo/tests/utilities/Driver.java
@@ -1,14 +1,58 @@
 package com.cydeo.tests.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    private Driver(){}
+    private Driver() {
+    }
 
     private static WebDriver driver;
 
+    public static WebDriver getDriver() {
 
+        if (driver == null) {
+
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+            }
+        }
+
+        return driver;
+    }
 
 
 }
