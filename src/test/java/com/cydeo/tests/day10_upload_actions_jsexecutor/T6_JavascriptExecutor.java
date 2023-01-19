feb85dfadb869e3552c30bd6a4d83f8668554e45
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-19 18:09:47 +0300

    Day 10 T6_JavascriptExecutor

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.Driver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
@@ -7,13 +8,25 @@ import org.testng.annotations.Test;
 public class T6_JavascriptExecutor {
 
     @Test
-    public void javascript_executor_test1(){
+    public void javascript_executor_test1() {
 
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
 
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
-        js.executeScript("window.scrollBy(0,0)");
+        for (int i = 0; i < 10; i++) {
+
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,750)");
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+
+        }
 
 
     }
