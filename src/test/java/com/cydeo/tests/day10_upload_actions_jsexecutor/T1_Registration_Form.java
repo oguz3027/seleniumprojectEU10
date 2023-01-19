9cbc56bc3e57238dbbba4c8e9599a2a5cf576adf
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-19 16:15:21 +0300

    Day 10 T1_Registration_Form

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -19,6 +19,30 @@ public class T1_Registration_Form {
         WebElement inputFirstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
         inputFirstName.sendKeys(faker.name().firstName());
 
+        WebElement inputLastName = Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
+        inputLastName.sendKeys(faker.name().lastName());
+
+        WebElement inputUserName = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+        //inputUserName.sendKeys(faker.name().username().replaceAll(".",""));
+        inputUserName.sendKeys(faker.bothify("helpdesk###"));
+
+        WebElement inputEmail = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        inputEmail.sendKeys(faker.bothify("helpdesk###")+"@email.com");
+
+        WebElement inputPassword = Driver.getDriver().findElement(By.xpath("//input[@name='password']"));
+        inputPassword.sendKeys(faker.numerify("#########"));
+
+        WebElement inputPhone = Driver.getDriver().findElement(By.xpath("//input[@name='phone']"));
+        inputPhone.sendKeys(faker.numerify("###-###-####"));
+
+        WebElement inputGender = Driver.getDriver().findElement(By.xpath("//input[@value='female']"));
+        inputGender.click();
+
+        WebElement inputBirthday = Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
+        // faker.number().numberBetween(2004,1920);
+
+
+
 
     }
 
