acf0eedfcdbf1fa573c08c11580cfce0b3ff716f
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-19 17:24:23 +0300

    Day 10 T3_Actions_Hover

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -0,0 +1,25 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import com.cydeo.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T3_Actions_Hover {
+
+    @Test
+    public void hovering_test(){
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
+
+
+
+    }
+}
