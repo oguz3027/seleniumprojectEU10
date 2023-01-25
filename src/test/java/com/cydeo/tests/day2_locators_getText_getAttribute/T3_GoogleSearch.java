cdc198b8d46297e8c4bff59048c107a038036647
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:37:33 +0300

    Day2 T3_GoogleSearch

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -6,22 +6,37 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class T3_GoogleSearch {
     public static void main(String[] args) {
 
+
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("intl.accept_languages", "en-GB");
+        options.setExperimentalOption("prefs", prefs);
         WebDriverManager.chromedriver().setup();
-        WebDriver driver  = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
+
+        //WebDriver driver  = new ChromeDriver();
+        //driver.get("https://www.google.com");
 
         WebElement googleSearchBox = driver.findElement(By.name("q"));
 
         googleSearchBox.sendKeys("apple"+ Keys.ENTER);
 
         String actualTitle = driver.getTitle();
-        String expectedTitle = "apple";
+        String expectedTitle = "apple - Google Search";
 
-        if(actualTitle.startsWith(expectedTitle)){
+        if(actualTitle.equals(expectedTitle)){
             System.out.println("Google search verification PASSED");
         }else{
             System.out.println("Google search verification FAILED");
