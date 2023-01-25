ecd1ebd92959463c1e0d996a8c230fdc13f6d959
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:36:26 +0300

    Day7 T1_WindowHandling

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,13 +1,11 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.Set;
 
 public class T1_WindowHandling {
 
@@ -29,24 +27,7 @@ public class T1_WindowHandling {
         ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
-        Set<String> allWindowHandles = driver.getWindowHandles();
-
-        for (String each : allWindowHandles) {
-
-            driver.switchTo().window(each);
-
-            System.out.println("Current URL : " + driver.getCurrentUrl());
-
-            if (driver.getCurrentUrl().contains("etsy")){
-                break;
-            }
-
-        }
-
-        String actualTitle = driver.getTitle();
-        String expectedTitle = "Etsy";
-
-        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        BrowserUtils.switchWindowAndVerify(driver,"etsy","Etsy");
 
 
     }
