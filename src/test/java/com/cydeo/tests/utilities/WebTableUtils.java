d615341e50eded68fb5a8641e27fed07a6526803
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:32:06 +0300

    utilities WebTableUtils

--- src/test/java/com/cydeo/tests/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/tests/utilities/WebTableUtils.java
@@ -3,16 +3,27 @@ package com.cydeo.tests.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableUtils {
 
-    public static String returnOrderDate(WebDriver driver,String customerName){
+    public static String returnOrderDate(WebDriver driver, String customerName) {
 
-        String locator = "//td[.='"+customerName+"']/following-sibling::td[3]";
+        String locator = "//table[@id='ctl00_MainContent_orderGrid']//td[.='" + customerName + "']/following-sibling::td[3]";
 
         WebElement customerDateCell = driver.findElement(By.xpath(locator));
 
         return customerDateCell.getText();
+
+    }
+
+    public static void orderVerify(WebDriver driver, String customerName, String expectedOrderDate){
+        String locator = "//table[@id='ctl00_MainContent_orderGrid']//td[.='" + customerName + "']/following-sibling::td[3]";
+
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        String actualOrderDate = customerDateCell.getText();
+        Assert.assertEquals(actualOrderDate,expectedOrderDate);
 
     }
 
