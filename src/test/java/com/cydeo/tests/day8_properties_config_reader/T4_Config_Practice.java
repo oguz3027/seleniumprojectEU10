82e37fce9c99610136a7586b13f612a9e87127e1
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:37:50 +0300

    Day8 T4_Config_Practice

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import com.cydeo.tests.utilities.ConfigurationReader;
+import com.cydeo.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T4_Config_Practice {
+
+    WebDriver driver;
+/*
+    @BeforeMethod
+    public void setupMethod(){
+
+        //WebDriverFactory.setupMethod(driver);
+
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+
+    }*/
+
+    @Test
+    public void google_search_test(){
+
+        Driver.getDriver().get("https://google.com");
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+
+        googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "apple - Google Search";
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+    }
+
+
+}
