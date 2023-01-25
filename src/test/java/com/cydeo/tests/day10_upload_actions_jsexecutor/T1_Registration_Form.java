f86c9d1c2c95e637752dd4799cb4d1a4f98be02c
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:35:52 +0300

    Day10 T1_Registration_Form

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -5,6 +5,8 @@ import com.cydeo.tests.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
@@ -39,7 +41,30 @@ public class T1_Registration_Form {
         inputGender.click();
 
         WebElement inputBirthday = Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
-        // faker.number().numberBetween(2004,1920);
+        //faker.number().numberBetween(2004,1920);
+        inputBirthday.sendKeys(faker.number().numberBetween(1,12)+"/"+
+                faker.number().numberBetween(1,31)+"/"+faker.number().numberBetween(1900,2022));
+
+        Select departmentDropdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='department']")));
+        departmentDropdown.selectByIndex(faker.number().numberBetween(1,9));
+
+        Select jobTitleDropdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='job_title']")));
+        jobTitleDropdown.selectByIndex(faker.number().numberBetween(1,8));
+
+        WebElement programmingLanguage1 = Driver.getDriver().findElement(By.xpath("//input[@value='java']"));
+        programmingLanguage1.click();
+
+        WebElement signupButton = Driver.getDriver().findElement(By.xpath("//button[@id='wooden_spoon']"));
+        signupButton.click();
+
+        WebElement successful = Driver.getDriver().findElement(By.xpath("//div[@class='alert alert-success']/p"));
+        String actual = successful.getText();
+        String expected = "You've successfully completed registration!";
+
+        Assert.assertEquals(actual,expected);
+
+
+
 
 
 
