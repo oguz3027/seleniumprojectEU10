82e90708a8820a6646bd561e589ba87c118c9442
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:33:13 +0300

    Day5 T3

---
+++ src/test/java/com/cydeo/myPractices/Day5_T3.java
@@ -0,0 +1,39 @@
+package com.cydeo.myPractices;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
+public class Day5_T3 {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+        clickAndVerifyRadioButton(driver, "sport", "hockey");
+
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue) {
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        List<WebElement> sportElement = driver.findElements(By.name(nameAttribute));
+
+        for (WebElement each : sportElement) {
+
+            String eachId = each.getAttribute("id");
+
+            if (eachId.equals(idValue)) {
+                each.click();
+                System.out.println("each.isDisplayed() = " + each.isDisplayed());
+                break;
+            }
+
+        }
+
+
+    }
+}
