d14e00faafca8783de526bf08cc46654466bd449
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:37:20 +0300

    Day7 T3_CRM_LOGIN

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,24 +1,15 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.CRM_Utilities;
-import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class T3_CRM_LOGIN {
+public class T3_CRM_LOGIN extends TestBase{
 
-    WebDriver driver;
 
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        WebDriverFactory.maximize(driver);
-        WebDriverFactory.implicitlyWait(driver);
-    }
 
     @Test
     public void crm_login_test(){
