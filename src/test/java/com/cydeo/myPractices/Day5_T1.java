835dd44830e79e7d28dc71947caf3790622364d2
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:32:52 +0300

    Day5 T1

---
+++ src/test/java/com/cydeo/myPractices/Day5_T1.java
@@ -0,0 +1,38 @@
+package com.cydeo.myPractices;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Day5_T1 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
+        WebElement addButton = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+        addButton.click();
+
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@onclick='deleteElement()']"));
+
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
+        deleteButton.click();
+
+        try{
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+        }catch (StaleElementReferenceException e){
+            System.out.println("--> StaleElementReferenceException exception is thrown");
+            System.out.println("-->This means the web element is completely deleted from the page");
+            System.out.println("deleteButton.isDisplayed() = false");
+        }
+
+    }
+
+}
