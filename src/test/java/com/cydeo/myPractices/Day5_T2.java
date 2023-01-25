dd3636cbdfd5f6d6da3aa15a8cf5c344fe220747
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:33:03 +0300

    Day5 T2

---
+++ src/test/java/com/cydeo/myPractices/Day5_T2.java
@@ -0,0 +1,27 @@
+package com.cydeo.myPractices;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Day5_T2 {
+    public static void main(String[] args) {
+
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        WebElement hockeyRadioButton = driver.findElement(By.xpath("//input[@id='hockey']"));
+
+        System.out.println("hockeyRadioButton.isSelected() = " + hockeyRadioButton.isSelected());
+
+        hockeyRadioButton.click();
+
+        System.out.println("hockeyRadioButton.isSelected() = " + hockeyRadioButton.isSelected());
+
+    }
+}
