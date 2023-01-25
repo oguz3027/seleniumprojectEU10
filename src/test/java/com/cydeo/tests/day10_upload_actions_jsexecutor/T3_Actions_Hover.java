c64679937319d34f3bfd0a6e54a7e8973629bf7b
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:37:00 +0300

    Day10 T3_Actions_Hover

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_Actions_Hover {
@@ -18,6 +21,24 @@ public class T3_Actions_Hover {
         WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user1']"));
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img1).perform();
+
+        Assert.assertTrue(user1.isDisplayed());
+
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img2).perform();
+
+        Assert.assertTrue(user2.isDisplayed());
+
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img3).perform();
+
+        Assert.assertTrue(user3.isDisplayed());
+
 
 
 
