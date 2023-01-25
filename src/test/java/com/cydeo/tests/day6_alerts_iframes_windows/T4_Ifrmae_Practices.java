0c14f8f04f534f35573f94e8516fd1a840493198
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-25 12:38:03 +0300

    Day6 T4_Ifrmae_Practices

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_Ifrmae_Practices.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class T4_Ifrmae_Practices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        WebDriverFactory.maximize(driver);
+        WebDriverFactory.implicitlyWait(driver);
+    }
+
+    @Test
+    public void iframe_test(){
+
+        driver.get("https://practice.cydeo.com/iframe");
+
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+
+        WebElement yourContentGoesHere = driver.findElement(By.xpath("//p"));
+
+        Assert.assertTrue(yourContentGoesHere.isDisplayed());
+
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        Assert.assertTrue(headerText.isDisplayed());
+
+
+    }
+}
