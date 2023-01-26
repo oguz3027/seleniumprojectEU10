47c43da79a5c0011067f9d571a36b289e28ab0c7
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-27 00:45:26 +0300

    Day11 T1_Circle_Drag_N_Drop

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.tests.utilities.BrowserUtils;
+import com.cydeo.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T1_Circle_Drag_N_Drop {
+
+    @Test
+    public void drag_and_drop_test(){
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+
+        Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler")).click();
+
+        WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
+        WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        BrowserUtils.sleep(2);
+        //actions.dragAndDrop(smallCircle,bigCircle).perform();
+        actions.clickAndHold(smallCircle).moveToElement(bigCircle).release().perform();
+
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText,expectedBigCircleText);
+
+
+
+
+    }
+
+
+
+
+}
