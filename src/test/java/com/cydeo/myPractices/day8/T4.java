26ebf56be5f11c12fd6238a37e0ecc80f1505554
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-02-13 12:05:36 +0300

    T4.java

--- src/test/java/com/cydeo/myPractices/day8/T4.java
+++ src/test/java/com/cydeo/myPractices/day8/T4.java
@@ -1,7 +1,7 @@
 package com.cydeo.myPractices.day8;
 
-import com.cydeo.tests.utilities.ConfigurationReader;
-import com.cydeo.tests.utilities.Driver;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
