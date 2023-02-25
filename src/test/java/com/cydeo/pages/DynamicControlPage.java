7aaa5b6fd57a7dcbaa1c455797fab5945fb5c2ba
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-02-25 19:09:38 +0300

    DynamicControlPage

---
+++ src/test/java/com/cydeo/pages/DynamicControlPage.java
@@ -0,0 +1,27 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicControlPage {
+    public DynamicControlPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(css = "form#checkbox-example > button")
+    public WebElement removeButton;
+
+    @FindBy(css = "div#loading")
+    public WebElement loadingBar;
+
+    @FindBy(css = "input[type='checkbox']")
+    public WebElement checkbox;
+
+    @FindBy(css = "p[id='message']")
+    public WebElement message;
+    
+
+}
